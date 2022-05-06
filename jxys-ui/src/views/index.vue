1a7da92dffc69d958ab37054d7c1b3ee24520658
李建 <a2417978181@163.com>
2022-05-06 11:48:45 +0800

    前端初始化

---
+++ jxys-ui/src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="width: 100%;">
+  </div>
+</template>
+<script>
+export default {
+  name: "Index",
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
+
