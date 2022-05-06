16a799d63e5fee336848eaeaeb86af38c62bcc6c
李建 <a2417978181@163.com>
2022-05-06 16:35:23 +0800

    初始化完成

--- jxys-common/src/main/java/com/jxys/common/config/MyMetaObjectHandler.java
+++ jxys-common/src/main/java/com/jxys/common/config/MyMetaObjectHandler.java
@@ -32,7 +32,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         // 若要使用精确到秒级的时间戳请将getCurrentUnixTimestamp()改成System.currentTimeMillis()并在数据库中设置long类型的create_time字段
         this.setFieldValByName("createTime", getCurrentUnixTimestamp(), metaObject);
         //存入userId
-        this.setFieldValByName("createBy", SecurityUtils.getLoginUser().getUserId(), metaObject);
+        this.setFieldValByName("createBy", SecurityUtils.getLoginUser().getUsername(), metaObject);
     }
 
     /**
@@ -44,7 +44,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         // 若要使用精确到秒级的时间戳请将getCurrentUnixTimestamp()改成System.currentTimeMillis()并在数据库中设置long类型的create_time字段
         this.setFieldValByName("updateTime", getCurrentUnixTimestamp(), metaObject);
         //存入userId
-        this.setFieldValByName("updateBy", SecurityUtils.getLoginUser().getUserId(), metaObject);
+        this.setFieldValByName("updateBy", SecurityUtils.getLoginUser().getUsername(), metaObject);
     }
 
 
