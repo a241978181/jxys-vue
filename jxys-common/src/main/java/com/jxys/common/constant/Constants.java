a9d72c97e9234bdfc3c3d34619e4c077c3b422ef
李建 <a2417978181@163.com>
2022-08-24 09:52:26 +0800

    若依-配合官方同步更新漏洞-2022年8月24日

--- jxys-common/src/main/java/com/jxys/common/constant/Constants.java
+++ jxys-common/src/main/java/com/jxys/common/constant/Constants.java
@@ -26,6 +26,12 @@ public class Constants{
     public static final String GBK = "GBK";
 
     /**
+     * www主域
+     */
+    public static final String WWW = "www.";
+
+
+    /**
      * http请求
      */
     public static final String HTTP = "http://";
